f5ebfd8eabdc32d63d644665cf430963071091e6
ramon <ramon.palao@gmail.com>
2020-02-12 01:24:30 +0100

    add retrieve player test, retrieve player details test, and toggle team fav test

--- staff/groups/maniacos/football-mania/football-mania-app/logic/retrieve-players.js
+++ staff/groups/maniacos/football-mania/football-mania-app/logic/retrieve-players.js
@@ -1,5 +1,6 @@
 function retrievePlayers(strTeam, callback) {
-    //debugger
+    if (typeof strTeam !== "string") throw new TypeError(`${strTeam} is not a string`)
+    if (!strTeam.trim()) throw new Error(`team name is empty`)
     if (typeof callback !== 'function') throw new TypeError(`${callback} is not a function`)
     
     call(`https://www.thesportsdb.com/api/v1/json/1/searchplayers.php?t=${strTeam}&p=`, undefined, (error, response) => {

e6a30af197c253fe338aeb1e33fd50fdab800fb1
ramon <ramon.palao@gmail.com>
2020-02-12 19:03:58 +0100

    add landing page with its style, and players style

--- staff/groups/maniacos/football-mania/football-mania-app/components/login.jsx
+++ staff/groups/maniacos/football-mania/football-mania-app/components/login.jsx
@@ -5,12 +5,12 @@ function Login({ onLogin }) {
         const password = event.target.password.value
         onLogin(username, password)
     }}>
-        <h2>Login</h2>
-        <input type="text" name="username" placeholder="username" />
-        <input type="password" name="password" placeholder="password" />
-        <button>login</button>
-
+        <section className="login__logo"></section>
+        <section className="login__container">
+            <h2 className="login__title">LOGIN</h2>
+            <input className="login__username" type="text" name="username" placeholder="username" />
+            <input className="login__password" type="password" name="password" placeholder="password" />
+            <button className="login__button">OK</button>
+        </section>
     </form>
-
-
 }

6330a470bb2fe7dae4fc693dd666eb9a4be69ebe
ramon <ramon.palao@gmail.com>
2020-02-13 16:08:55 +0100

    add team badges to position table

--- staff/groups/maniacos/football-mania/football-mania-app/components/result-table.jsx
+++ staff/groups/maniacos/football-mania/football-mania-app/components/result-table.jsx
@@ -1,9 +1,10 @@
-function ResultTable({table, onToResults}){
+function ResultTable({table, onToResults, detail, teams}){
     return <ul className="table__container">
         <a href="" onClick={event=>{
             event.preventDefault()
             onToResults()
         }}>Go back to Results</a>
-        {table.map((team, index) => <TablePosition key={index} table={team} />)}
+        {table.map((team, index) => <TablePosition key={index} table={team} detail={detail} teams={teams}/>)}
+        {teams.map((team, index) => <TablePosition key={index} table={team} detail={detail} teams={teams}/>)}
     </ul>
 }

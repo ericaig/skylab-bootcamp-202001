39d67bb5a34b9ebbcf9df06f62ea1d1a092dd8ae
Eric Aig <>
2020-02-14 12:31:40 +0100

    added retrieve table test

--- staff/groups/maniacos/football-mania/football-mania-app/logic/retrieve-table.js
+++ staff/groups/maniacos/football-mania/football-mania-app/logic/retrieve-table.js
@@ -1,16 +1,14 @@
 /**
- * @param {string} idTeam - Team's ID
  * @param {function} callback - Callback to execute after retrieving a leagues current team's ranking. It returns error and an array with objects
  * @throws Will throw an error if an argument doesn't match it's type
  */
 
-function retrieveTable(idTeam, callback) {
-    if (typeof callback !== "function") throw new TypeError(`callback ${callback} is not a function`)
+function retrieveTable(callback) {
+    if (typeof callback !== "function") throw new TypeError(`${callback} is not a function`)
 
     call(`https://www.thesportsdb.com/api/v1/json/1/lookuptable.php?l=4335&s=1920`, undefined, (error, response) => {
         if (error) return callback(error, response)
         const content = JSON.parse(response.content)
-
-        callback(idTeam, (error, content.table))
+        callback(error, content.table)
     })
 }

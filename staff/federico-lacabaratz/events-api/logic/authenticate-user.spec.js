3fc2aea567c7e6cded40beff92b401689c921bf8
Federico Lacabaratz <federicolacabaratz@MacBook-Pro-de-Federico.local>
2020-02-27 18:36:06 +0100

    add update to challenge before mongodb

--- staff/federico-lacabaratz/events-api/logic/authenticate-user.spec.js
+++ staff/federico-lacabaratz/events-api/logic/authenticate-user.spec.js
@@ -1,1 +1,33 @@
-// TODO
+const { authenticateUser } = require('.')
+const {expect} = require('chai')
+
+
+describe('authenticate', () => {
+    let user
+
+    beforeEach(() => {
+        users.length = 0
+
+        user = {
+            name: `name-${Math.random()}`,
+            surname: `surname-${Math.random()}`,
+            email: `${Math.random()}@email.com`,
+            password: `password-${Math.random()}`,
+
+        }
+    })
+
+    describe('when user already exists', () => {
+        beforeEach(() => {
+            users.push(user)
+        })
+
+        it('should succeed on correct credentials', () =>
+            authenticateUser(email, password)
+                .then(id => {
+                    expect(user.id).to.be.a('string')
+                })
+
+        )
+    })
+})
